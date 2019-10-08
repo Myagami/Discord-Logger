b2946f3f9839a5efd5500a53ba2f30bab1b8797c
M.Kousaka <helloproject.odoru10th@gmail.com>
2019-10-08 22:26:05 +0900

    add sample msg

--- Discord_Logger.rb
+++ Discord_Logger.rb
@@ -1,15 +1,38 @@
+require 'net/https'
+require 'uri'
+require 'json'
+
 class Discord_Logger
-    @POST_URL #webhook post url
+    #@POST_URL #webhook post url
+    @URI
+    @HTTP_Connect
+    @Content
     def initialize(url)
        puts "Discord Logger Lib" 
-
-       @POST_URL = url
-
-       puts @POST_URL
+       @URI = URI.parse(url)
+       @HTTP_Connect = Net::HTTP::new(@URI.host,@URI.port)
+       @HTTP_Connect.use_ssl = true
     end
 
-    def SendUser(user)
+    def SetContent(content)
+        @Content = content
+    end
+
+    def GetParam
+        puts "Content:"+@Content
         
-    end 
+    end
+
+
+
+    def Send
+        @HTTP_Connect.start do 
+            req = Net::HTTP::Post.new(@URI.path)
+            req["Content-Type"] = "application/json"
+            ts = {"content":"ねこです よろしくおねがいします","username":"SCP-040-JP"}
+            req.body = ts.to_json
+            @HTTP_Connect.request(req)
+        end
+    end
 
 end
