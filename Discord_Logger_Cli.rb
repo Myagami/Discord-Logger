6ede6b8d8fe8acdd44b3f44113db72b2df8c382c
M.Kousaka <helloproject.odoru10th@gmail.com>
2019-10-08 22:26:13 +0900

    method run

--- Discord_Logger_Cli.rb
+++ Discord_Logger_Cli.rb
@@ -2,7 +2,8 @@
 
 require_relative 'Discord_Logger'
 
-DisLog = Discord_Logger.new("test")
-
-
-
+DisLog = Discord_Logger.new("https://discordapp.com/api/webhooks/630666840200445952/AmCvkJrG--WZL0MHvga6FvzIVq0XSTnj65SKakmPWKNSdGqXAVbHBA4BP6Kq4KAr1F5c")
+#DisLog.SendingUser("Clowler IL-334")
+DisLog.SetContent("Secure Contaiment Protect")
+DisLog.GetParam
+DisLog.Send
