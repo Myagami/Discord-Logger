72dae0e833399ec2fb9e2508ec9c703107ed3543
M.Kousaka <helloproject.odoru10th+github@gmail.com>
2019-10-08 22:34:02 +0900

    Update Discord_Logger_Cli.rb

--- Discord_Logger_Cli.rb
+++ Discord_Logger_Cli.rb
@@ -2,7 +2,7 @@
 
 require_relative 'Discord_Logger'
 
-DisLog = Discord_Logger.new("https://discordapp.com/api/webhooks/630666840200445952/AmCvkJrG--WZL0MHvga6FvzIVq0XSTnj65SKakmPWKNSdGqXAVbHBA4BP6Kq4KAr1F5c")
+DisLog = Discord_Logger.new("URL")
 #DisLog.SendingUser("Clowler IL-334")
 DisLog.SetContent("Secure Contaiment Protect")
 DisLog.GetParam
