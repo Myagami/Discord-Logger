e284cfc12b5ae3fd89f6d8f4d9287c496ff86ea6
M.Kousaka <helloproject.odoru10th@gmail.com>
2019-10-09 14:27:56 +0900

    embed test

--- Discord_Logger.rb
+++ Discord_Logger.rb
@@ -7,6 +7,7 @@ class Discord_Logger
     @URI
     @HTTP_Connect
     @Content
+    @UserName
     def initialize(url)
        puts "Discord Logger Lib" 
        @URI = URI.parse(url)
@@ -18,8 +19,17 @@ class Discord_Logger
         @Content = content
     end
 
+    def SetUserName(name)
+        @UserName = name
+    end
+
+    def Embed_MSG
+
+    end
+
     def GetParam
-        puts "Content:"+@Content
+        puts "UserName:"+@UserName
+        puts "Content:"+@Content.to_s
         
     end
 
@@ -29,7 +39,38 @@ class Discord_Logger
         @HTTP_Connect.start do 
             req = Net::HTTP::Post.new(@URI.path)
             req["Content-Type"] = "application/json"
-            ts = {"content":"ねこです よろしくおねがいします","username":"SCP-040-JP"}
+            ts = {#"content":"#{@Content.to_s.gsub(/\\n/,"\n")}",
+                    "username":"#{@UserName}",
+                    "embeds":[{
+                            "title":"SCP-040-JP - ねこです よろしくおねがいします",
+                            "description":"SCP-JP 当該報告書ページ",
+                            "url":"http://ja.scp-wiki.net",
+                            "footer":{
+                                "text":"Author:ikr-4185"
+                            },
+                            "author":{
+                                "name":"SCP-040-JP",
+                                "url":"http://ja.scp-wiki.net/scp-040-JP"
+
+                            },
+                            "fields": [
+                                {
+                                  "name": ":thumbsup:Vote",
+                                  "value": "40",
+                                  "inline":true
+                                },
+                                {
+                                  "name": "ディスカッション",
+                                  "value": "280",
+                                  "inline":true
+                                },
+                                {
+                                  "name": ":bookmark_tabs:タグ",
+                                  "value": "ミーム,財団製,Thaumiel",
+                                  "inline":true
+                                }
+                              ]
+                            }]}
             req.body = ts.to_json
             @HTTP_Connect.request(req)
         end
