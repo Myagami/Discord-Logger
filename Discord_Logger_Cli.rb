527b01cbab19a947381dbf8144d45e389192b854
M.Kousaka <helloproject.odoru10th@gmail.com>
2019-10-09 14:27:47 +0900

    msg args

--- Discord_Logger_Cli.rb
+++ Discord_Logger_Cli.rb
@@ -1,9 +1,28 @@
 #!/usr/bin/env ruby
 
 require_relative 'Discord_Logger'
+require 'optparse'
+option = {}
+OptionParser.new do |opt|
+    opt.on('-p value','role') {|val|
+        @url = val
+    }
 
-DisLog = Discord_Logger.new("URL")
+    opt.on('-c value','content'){|val|
+        @content = val
+    }
+
+    opt.on('-n value','name') {|val|
+        @userName = val
+    }
+
+    opt.parse!(ARGV)
+end
+
+
+DisLog = Discord_Logger.new(@url)
 #DisLog.SendingUser("Clowler IL-334")
-DisLog.SetContent("Secure Contaiment Protect")
+DisLog.SetContent(@content)
+DisLog.SetUserName(@userName)
 DisLog.GetParam
 DisLog.Send
